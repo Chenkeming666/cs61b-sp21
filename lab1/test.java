04b082c0cc38ed3a1a656946156449f9fb9114d9
ChenKeming <qq3014209107@gmail.com>
2023-12-18 20:11:51 +0800

    submit-5

--- lab1/test.java
+++ lab1/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("this is test and look for guanjianci");
+        System.out.println("this is test 2");
     }
 }
