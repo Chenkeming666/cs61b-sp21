bd56bc648ae92fe95cddfa1622140eb96d971124
ChenKeming <qq3014209107@gmail.com>
2023-12-18 20:12:54 +0800

    delete1

--- lab1/test.java
+++
@@ -1,6 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println("this is test and look for guanjianci");
-        System.out.println("this is test 2");
-    }
-}
